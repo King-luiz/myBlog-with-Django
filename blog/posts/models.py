68c7a97a1c4f616139f9a906d9ee122363046b4a
King-luiz <kingluiz47@gmail.com>
2026-02-06 13:30:10 +0300

    initial commit

---
+++ blog/posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
+    created_at = models.DateTimeField(default= datetime.now)
+    def __str__(self):
+        return self.title
+   
